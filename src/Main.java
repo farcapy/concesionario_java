14ab4e9b12046b52439584f137791b3aad1020ee
fredyarca15 <fredyarcawpy@gmail.com>
2024-08-25 21:30:16 -0400

    FormVehiculo creado nuevamente despues de haber sido borrado accidentalmente.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+import form.FormVehiculo;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        FormVehiculo form = new FormVehiculo();
+        form.show();
+        
+//        FormularioCliente fc = new FormularioCliente();
+//        fc.show();
+    }
+}
